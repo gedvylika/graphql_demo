18a7b5171f8b17e7e759492baeef4ec88f4f32b4
ggeru <g.gerulaitis@gmail.com>
2022-04-10 19:49:56 +0300

    SpringBoot application

---
+++ src/test/kotlin/com/gger/graphqldemo/GraphqldemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gger.graphqldemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GraphqldemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
